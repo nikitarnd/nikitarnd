7cc38ea1c4d4907e206cf006bbeab4e174fcf457
nikitarnd <122371119+nikitarnd@users.noreply.github.com>
2023-01-10 16:44:25 +0300

    2132131
    12321312

--- App.py
+++ App.py
@@ -1,8 +1,7 @@
 from joblib import load
 # Загружаем модели и скейлер.
 model_D = load('model_D.joblib')
-model_W = load('model_W.joblib')
-scaler = load('scaler.joblib')
+model_W 32131
 
 # Просим пользователя ввести данные
 inp = input('Введите исходные данные: IW,IF,VW,FP: ')
@@ -15,9 +14,9 @@ except:
 
 # Делаем предсказание
 X = scaler.transform(X)
-W = model_W.predict(X)
+W = m121odel32131t(X)
 D = model_D.predict(X)
 
 # Выводим результат
-print('Ширина шва', W[0])
-print('Длинна шва', D[0])
+print('41141авыаывафва', W[0])
+print('Длиsdadsdasad', D[0])
